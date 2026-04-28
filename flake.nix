70a0300d42156e938dd28e2a0c0bbdcfebc13e2d
bgl gwyng <bgl@gwyng.com>
2026-04-28 22:40:01 +0900

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        # To import an internal flake module: ./other.nix
+        # To import an external flake module:
+        #   1. Add foo to inputs
+        #   2. Add foo as a parameter to the outputs function
+        #   3. Add here: foo.flakeModule
+
+      ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          devShells = {
+            rust = pkgs.mkShell {
+              nativeBuildInputs = [
+                (pkgs.rust-bin.stable."1.95.0".default.override { extensions = [ "rust-src" ]; })
+              ];
+            };
+            haskell = pkgs.mkShell {
+              packages = with pkgs; [
+                ghc
+                cabal-install
+                haskell-language-server
+              ];
+            };
+          };
+        };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+      };
+    };
+}
